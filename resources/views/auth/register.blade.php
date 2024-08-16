5695825b70ef1845a15cb578fbccf3afcc9cf7fc
brenodahora <breno-dahora@hotmail.com>
2024-08-16 13:39:33 -0300

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,38 +3,95 @@
 @section('title', 'Cadastro - PayNet')
 
 @section('content')
-    <form name="formRegister" autocomplete="off">
-        @csrf
+    <div class="container m-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="card p-4 shadow-lg">
 
-        <div class="alert alert-danger d-none alertResponse" role="alert"></div>
+                    <h2 class="text-center">Cadastro de Usuário</h2>
 
-        <label>Nome: </label>
-        <input type="text" name="name" id="name" placeholder="Nome completo" required><br><br>
+                    <form name="formRegister" autocomplete="off" class="mt-4">
+                        @csrf
 
-        <label>e-mail: </label>
-        <input type="email" name="email" id="email" required><br><br>
+                        <div class="alert alert-danger d-none alertResponse" role="alert"></div>
 
-        <label>Senha: </label>
-        <input type="password" name="password" id="password" required><br><br>
+                        <div class="form-group mt-3">
+                            <label for="name">Nome</label>
+                            <input type="text" name="name" id="name" class="form-control"
+                                placeholder="Nome completo" required>
+                        </div>
 
-        <label>Confirmar senha: </label>
-        <input type="password" name="password_confirm" id="password_confirm" required><br><br>
+                        <div class="form-group mt-3">
+                            <label for="email">e-mail</label>
+                            <input type="email" name="email" id="email" class="form-control" required>
+                        </div>
 
-        <label>CEP: </label>
-        <input type="text" name="zipcode" id="zipcode" required><br><br>
-        <label>Rua: </label>
-        <input type="text" name="street" id="street" required><br><br>
-        <label>Bairro: </label>
-        <input type="text" name="neighborhood" id="neighborhood" required><br><br>
-        <label>Número: </label>
-        <input type="text" name="number" id="number" required><br><br>
-        <label>Cidade: </label>
-        <input type="text" name="city" id="city" required><br><br>
-        <label>Estado: </label>
-        <input type="text" name="state" id="state" required><br><br>
+                        <div class="row">
+                            <div class="col-md-6 mt-3 pb-2">
+                                <div data-mdb-input-init class="form-outline">
+                                    <label for="password">Senha</label>
+                                    <input type="password" name="password" id="password" class="form-control" required>
+                                </div>
+                            </div>
 
-        <button type="submit" class="btn btn-lg btn-primary btn-block">Cadastrar</button>
-    </form>
+                            <div class="col-md-6 mt-3 pb-2">
+                                <div data-mdb-input-init class="form-outline">
+                                    <label for="password_confirm">Confirmar senha</label>
+                                    <input type="password" name="password_confirm" id="password_confirm"
+                                        class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group mt-3">
+                                    <label for="zipcode">CEP</label>
+                                    <input type="text" name="zipcode" id="zipcode" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="form-group mt-3">
+                                    <label for="number">Número</label>
+                                    <input type="text" name="number" id="number" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group mt-3">
+                            <label for="street">Rua</label>
+                            <input type="text" name="street" id="street" class="form-control" required>
+                        </div>
+
+                        <div class="form-group mt-3">
+                            <label for="neighborhood">Bairro</label>
+                            <input type="text" name="neighborhood" id="neighborhood" class="form-control" required>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group mt-3">
+                                    <label for="city">Cidade</label>
+                                    <input type="text" name="city" id="city" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group mt-3">
+                                    <label for="state">Estado</label>
+                                    <input type="text" name="state" id="state" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex justify-content-center mt-3">
+                            <button type="submit" class="btn btn-lg btn-primary btn-block center mt-3">Cadastrar</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
         $(function() {
@@ -63,7 +120,8 @@
                             }
                         },
                         error: function(response) {
-                            $('.alertResponse').removeClass('d-none').html(response.responseJSON.message);
+                            $('.alertResponse').removeClass('d-none').html(response.responseJSON
+                                .message);
                         },
                     });
                 }
@@ -78,14 +136,15 @@
                     data: $(this).serialize(),
                     dataType: 'json',
                     success: function(response) {
-                        if (response.success == true) {
-                            window.location.href = "{{ route('home') }}"
+                        if (response.success) {
+                            window.location.href = "{{ route('home') }}";
                         } else {
                             $('.alertResponse').removeClass('d-none').html(response.message);
                         }
                     },
                     error: function(response) {
-                        $('.alertResponse').removeClass('d-none').html(response.responseJSON.message);
+                        $('.alertResponse').removeClass('d-none').html(response.responseJSON
+                            .message);
                     },
                 });
             });
